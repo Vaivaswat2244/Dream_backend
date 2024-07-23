6045544bec56e616151710bd08e7ae67d42cb068
Vaivaswat Dubey <113991324+Vaivaswat2244@users.noreply.github.com>
2024-07-23 14:31:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoute");
-const userDetailsRoutes = require("./routes/userRoute");
 const connectDB = require("./database/db");
-const checkUser = require("./middleware/checkUser");
-const cors = require("cors");
 
 connectDB();
 
