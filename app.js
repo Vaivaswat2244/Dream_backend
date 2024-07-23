f9218ace41c8b5dc031798a33be2f8caa8d459c6
Vaivaswat Dubey <113991324+Vaivaswat2244@users.noreply.github.com>
2024-07-23 14:30:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const connectDB = require("./database/dbconnect");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoute");
+const userDetailsRoutes = require("./routes/userRoute");
+const connectDB = require("./database/db");
+const checkUser = require("./middleware/checkUser");
+const cors = require("cors");
+
+connectDB();
+
+const app = express();
+app.use("/auth", authRoutes);
+app.get('/',(req,res)=>{
+  res.send("Dream Running")
+})
+
+app.listen(7000, function () {
+  console.log("Listening on port 7000");
+});
+
